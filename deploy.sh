b90e7cd8d08874d01cc8b3d9777277a445f768de
Ping Si (siping) <siping@cisco.com>
2023-03-28 17:27:28 +0800

    initial

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# abort on errors
 set -e
 
-# 生成静态文件
-npm run build
+# build
+npm run docs:build
 
-# 进入生成的文件夹
+# navigate into the build output directory
 cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:appsdevstudios/2023.git
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:appsdevstudios/2023.git main
 
 cd -
